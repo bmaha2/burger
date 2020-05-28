fe18a67f8d6b90ff10cbd1c6a539232f777613c5
bmaha2 <maharjanbipin@gmail.com>
2020-05-27 19:33:29 -0700

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Chicken Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon Cheese Burger', FALSE);
